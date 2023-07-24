ad4116e547b2b3b133c7a06f3bfbe11c58774ea9
Ram Kandasamy <ram@auderenow.org>
2023-07-24 12:08:42 -0700

    Use parent/child processes to manage restarts, not detached daemon

--- src/index.ts
+++ src/index.ts
@@ -20,7 +20,7 @@ function fatalError(error: string) {
     process.exitCode = 1;
 }
 
-function daemonMain(args: Args, onDoneInitializing: () => void) {
+function childMain(args: Args, onDoneInitializing: () => void) {
     process.on("uncaughtException", function (err) {
         fatalError(""+err);
         process.exit(1); // hard stop; can't rely on just process.exitCode
@@ -36,58 +36,23 @@ function daemonMain(args: Args, onDoneInitializing: () => void) {
 }
 
 function main(args: string[]) {
-    const DONE_INITIALIZING = "done_initializing";
-
-    // When bazels3cache launches, it spawns a child bazels3cache with "--daemon"
+    // When bazels3cache launches, it spawns a child bazels3cache with "--child"
     // added to the command line.
     //
-    // The parent process then waits until that child process either exits, or sends
-    // us a "done_initializing" message. Then the parent process exits.
-    if (args.indexOf("--daemon") === -1) {
+    // The parent process then restarts the child process if it exits, allowing it
+    // to reload the AWS token from the file. This was the easiest method I could
+    // find to automatically reload the token file if it changes.
+    if (args.indexOf("--child") === -1) {
         // We are parent process
-        const devnull = fs.openSync("/dev/null", "r+");
-        // As described here https://github.com/nodejs/node/issues/17592, although
-        // child_process.fork() doesn't officially support `detached: true`, it works
-        const child = child_process.fork(__filename, ["--daemon"].concat(args),
-            <any>{ detached: true });
 
-        // This is so that if we terminate *without* receiving a "done_initializig"
-        // message from the child process, that's because the child process
-        // terminated unexpectedly, so we should exit with an error code.
-        //
-        // While we're waiting, the child process still has stdout and stderr, and
-        // can send any messages to there.
-        process.exitCode = 1;
+        const child = child_process.fork(__filename, ["--child"].concat(args),
+            { silent: true });
 
-        child.on("message", msg => {
-            if (msg === DONE_INITIALIZING) {
-                child.unref(); // don't wait for the child process to terminate
-                child.disconnect(); // don't wait on the ipc channel any more
-                process.exitCode = 0; // now we can exit cleanly
-            }
-        });
+        child.stdout.on("data", data => console.log(Buffer.from(data).toString("utf-8")));
+        child.on("exit", () => main(args));
     } else {
         // child process
-        daemonMain(minimist<Args>(args), () => {
-            // Now that the daemon has finished initializing, we need to:
-            // - close stdin, stdout, and stderr, so that we don't keep these handles
-            //   open cause problems
-            // - open /dev/null for all three of those
-            // - send a "done_initializing" message to our parent process.
-
-            fs.closeSync(0);
-            fs.closeSync(1);
-            fs.closeSync(2);
-            // Odd: If I don't do the following, then sometimes writes such as
-            // console.log() still show up on the screen. I don't get it.
-            process.stdout.write = process.stderr.write = (): undefined => undefined;
-            fs.openSync("/dev/null", "r+"); // stdin
-            fs.openSync("/dev/null", "r+"); // stdout
-            fs.openSync("/dev/null", "r+"); // stderr
-
-            // Tells the parent that we initialized successfully and it can exit
-            process.send(DONE_INITIALIZING);
-        });
+        childMain(minimist<Args>(args), () => {});
     }
 }
 
